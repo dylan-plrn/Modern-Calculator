7b7b0ac22848e1260463d7ea5161e585e94532ae
Dylan <dylan-pleuron@hotmail.fr>
2021-08-28 09:54:12 +0200

    Moving calculator feature

--- Calculatrice.js
+++ Calculatrice.js
@@ -19,9 +19,47 @@ function clearScreen() {
 }
 
 
+// Moving calculator feature below
+
+let calculatorBlock = document.querySelector(".calculatorBlock");
+let mouseDragging = false;
+
+let checkPointMouseX;
+let checkPointMouseY;
+let checkPointBlockX;
+let checkPointBlockY;
+
+calculatorBlock.style.left= "50%";
+calculatorBlock.style.top= "80px";
 
 
+document.querySelector(".screenBlock").addEventListener("mousedown", (e) => {
+    mouseDragging = true;
+    checkPointMouseX = e.x;
+    checkPointMouseY = e.y;
+    checkPointBlockX = calculatorBlock.offsetLeft; 
+    checkPointBlockY = calculatorBlock.offsetTop;
 
+});
+
+document.querySelector("html").addEventListener("mousemove", (e) => {
+
+    if (mouseDragging === true) {
+        
+        calculatorBlock.style.left = checkPointBlockX + e.x - checkPointMouseX + "px";
+        calculatorBlock.style.top = checkPointBlockY + e.y - checkPointMouseY + "px";
+
+        console.log(calculatorBlock.offsetLeft);
+        console.log(calculatorBlock.offsetTop);
+    }
+    else {
+
+    }
+});
+
+document.querySelector("html").addEventListener("mouseup", (e) => {
+    mouseDragging = false;
+});
 
 
 
